53ce145a7d76ae4edb81b78a65647a105511dea4
David Khowanto <44863712+adavidkhowantolim@users.noreply.github.com>
2020-06-23 20:23:36 +0700

    Create TesMPR121.ino

---
+++ Minggu 2/TesMPR121/TesMPR121.ino
@@ -0,0 +1,50 @@
+#include "SPI.h"
+#include "Adafruit_GFX.h"
+#include "Adafruit_ILI9341.h"
+
+// For the Adafruit shield, these are the default.
+#define TFT_DC 4
+#define TFT_CS 15
+#define TFT_RST 2
+#define TFT_MISO 19         
+#define TFT_MOSI 23           
+#define TFT_CLK 18 
+
+// Use hardware SPI (on Uno, #13, #12, #11) and the above for CS/DC
+//Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+// If using the breakout, change pins as desired
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
+
+#include "mpr121.h"
+#include <Wire.h>
+// sda scl 21/22 atau 22/21
+int irqpin = 5;  // Digital 2
+boolean touchStates[12]; //to keep track of the previous touch states
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(irqpin, INPUT_PULLUP);
+//  digitalWrite(irqpin, HIGH); //enable pullup resistor
+  Serial.begin(9600);
+  Wire.begin();
+  tft.begin();
+  
+  mpr121_setup();
+  
+   // read diagnostics (optional but can help debug problems)
+  uint8_t x = tft.readcommand8(ILI9341_RDMODE);
+  Serial.print("Display Power Mode: 0x"); Serial.println(x, HEX);
+  x = tft.readcommand8(ILI9341_RDMADCTL);
+  Serial.print("MADCTL Mode: 0x"); Serial.println(x, HEX);
+  x = tft.readcommand8(ILI9341_RDPIXFMT);
+  Serial.print("Pixel Format: 0x"); Serial.println(x, HEX);
+  x = tft.readcommand8(ILI9341_RDIMGFMT);
+  Serial.print("Image Format: 0x"); Serial.println(x, HEX);
+  x = tft.readcommand8(ILI9341_RDSELFDIAG);
+  Serial.print("Self Diagnostic: 0x"); Serial.println(x, HEX);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  readTouchInputs();
+}
